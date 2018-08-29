401186086c32d78eeb9af4c5fe99781874c6632e
Jonathan <jonathanperalta1893@gmail.com>
2018-08-28 20:57:37 -0500

    pom

--- src/main/java/edu/moduloalumno/App.java
+++ src/main/java/edu/moduloalumno/App.java
@@ -13,9 +13,10 @@ public class App extends SpringBootServletInitializer{
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(App.class, args);
 	}
-           
+   /*        
         protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
             return application.sources(App.class);
         }
+        */
 }
 
