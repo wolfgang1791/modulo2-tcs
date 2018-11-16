a9542113d8d086a620256abef806bcb1b05a007a
Jonathan <jonathanperalta1893@gmail.com>
2018-11-15 19:05:02 -0500

    weaaaa

--- src/main/java/edu/moduloalumno/App.java
+++ src/main/java/edu/moduloalumno/App.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class App{// extends SpringBootServletInitializer{// Sin nada de esto para el herokud
+public class App{// extends SpringBootServletInitializer{// Sin nada de esto para el heroku
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(App.class, args);
