40342264b1978d64f57e414ad755cf34c26f98e8
Jonathan <jonathanperalta1893@gmail.com>
2018-11-23 13:56:50 -0500

    pruebita

---
+++ src/main/java/edu/moduloalumno/model/PruebaTCambio.java
@@ -0,0 +1,22 @@
+package edu.moduloalumno.model;
+
+public class PruebaTCambio {
+	
+	private float compra;
+	private float venta;
+	public float getCompra() {
+		return compra;
+	}
+	public void setCompra(float compra) {
+		this.compra = compra;
+	}
+	public float getVenta() {
+		return venta;
+	}
+	public void setVenta(float venta) {
+		this.venta = venta;
+	}
+	
+	
+
+}
